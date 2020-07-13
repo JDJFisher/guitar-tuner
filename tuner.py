3a4084373ec33bf0ed5442ba259888bc9160d7d9
Jordan Fisher <jordanfisher2096@gmail.com>
2020-07-13 19:29:36 +0100

    Increased note range

--- tuner.py
+++ tuner.py
@@ -12,44 +12,24 @@
 import numpy as np
 import pyaudio
 
-######################################################################
-# Feel free to play with these numbers. Might want to change NOTE_MIN
-# and NOTE_MAX especially for guitar/bass. Probably want to keep
-# FRAME_SIZE and FRAMES_PER_FFT to be powers of two.
 
-NOTE_MIN = 60       # C4
+NOTE_MIN = 47       # C4
 NOTE_MAX = 69       # A4
 FSAMP = 22050       # Sampling frequency in Hz
 FRAME_SIZE = 2048   # How many samples per frame?
 FRAMES_PER_FFT = 16 # FFT takes average across how many frames?
 
-######################################################################
-# Derived quantities from constants above. Note that as
-# SAMPLES_PER_FFT goes up, the frequency step size decreases (so
-# resolution increases); however, it will incur more delay to process
-# new sounds.
-
 SAMPLES_PER_FFT = FRAME_SIZE*FRAMES_PER_FFT
 FREQ_STEP = float(FSAMP)/SAMPLES_PER_FFT
+NOTE_NAMES = 'C D♭ D E♭ E F G♭ G A♭ A B♭ B'.split()
 
-######################################################################
-# For printing out notes
-
-NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
-
-######################################################################
-# These three functions are based upon this very useful webpage:
-# https://newt.phys.unsw.edu.au/jw/notes.html
 
 def freq_to_number(f): return 69 + 12*np.log2(f/440.0)
 def number_to_freq(n): return 440 * 2.0**((n-69)/12.0)
 def note_name(n): return NOTE_NAMES[n % 12] 
 
-######################################################################
-# Ok, ready to go now.
 
 # Get min/max index within FFT of notes we care about.
-# See docs for numpy.rfftfreq()
 def note_to_fftbin(n): return number_to_freq(n)/FREQ_STEP
 imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN-1))))
 imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
@@ -72,7 +52,6 @@ window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, SAMPLES_PER_FFT, False)))
 
 # Print initial text
 print('sampling at', FSAMP, 'Hz with max resolution of', FREQ_STEP, 'Hz')
-print
 
 # As long as we are getting data:
 while stream.is_active():
