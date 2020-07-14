3b9a82fbd738321b8fbaf30c19e03d2c38178ada
Jordan Fisher <jordanfisher2096@gmail.com>
2020-07-14 15:59:28 +0100

    Encapsulated tuner

--- tuner/__main__.py
+++ tuner/__main__.py
@@ -1,11 +1,21 @@
+#!/usr/bin/python
 
+# Standard library imports
 import argparse
+import signal
+import sys
+
+# Third party imports
 import pyaudio
 
-# import tuner
+# Local application imports
+from tuner import Tuner
 
 
 def main(argv=None):
+    # Bind signal handler
+    signal.signal(signal.SIGINT, signal_handler)
+
     # Collect all input device ids
     p = pyaudio.PyAudio()
     is_input = lambda id : p.get_device_info_by_host_api_device_index(0, id).get('maxInputChannels') > 0
@@ -14,19 +24,27 @@ def main(argv=None):
 
     # Parse args
     parser = argparse.ArgumentParser(prog='tuner')
-    parser.add_argument('-ld', '--listdevices', help='List input devices', action='store_true')
+    parser.add_argument('-v', '--verbose', help='Verbose', action='store_true')
+    parser.add_argument('-l', '--listdevices', help='List input devices', action='store_true')
     parser.add_argument('-d', '--device', help='Input device id', type=int, choices=in_devs)
     args = parser.parse_args(argv)
 
     # List input devices
     if args.listdevices:
-        print('id   name')
+        print('id    name')
         for id in in_devs:
             info = p.get_device_info_by_host_api_device_index(0, id)
-            print(f'{id}    {info.get("name")}')
+            print(f' {id}    {info.get("name")}')
 
+    # Run tuner
     else:
-        import tuner # TODO: implement
+        t = Tuner(args.device)
+        t.go(args.verbose)
+
+
+def signal_handler(sig, frame):
+    # TODO: cleanup
+    sys.exit(0)
   
 
 if __name__ == '__main__':
--- tuner/tuner.py
+++ tuner/tuner.py
@@ -6,9 +6,9 @@
 #          https://creativecommons.org/licenses/by-sa/3.0/us/
 ######################################################################
 
+# Third party imports
 import numpy as np
 import pyaudio
-
 
 NOTE_MIN = 47       # C4
 NOTE_MAX = 69       # A4
@@ -20,57 +20,72 @@ SAMPLES_PER_FFT = FRAME_SIZE*FRAMES_PER_FFT
 FREQ_STEP = float(FSAMP)/SAMPLES_PER_FFT
 NOTE_NAMES = 'C D♭ D E♭ E F G♭ G A♭ A B♭ B'.split()
 
-
 def freq_to_number(f): return 69 + 12*np.log2(f/440.0)
 def number_to_freq(n): return 440 * 2.0**((n-69)/12.0)
 def note_name(n): return NOTE_NAMES[n % 12]
-
-
-# Get min/max index within FFT of notes we care about.
 def note_to_fftbin(n): return number_to_freq(n)/FREQ_STEP
-imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN-1))))
-imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
 
-# Allocate space to run an FFT.
-buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
-num_frames = 0
+I_MIN = max(0, int(np.floor(note_to_fftbin(NOTE_MIN-1))))
+I_MAX = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
 
-# Initialize audio
-stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                channels=1,
-                                rate=FSAMP,
-                                input=True,
-                                output=False,
-                                input_device_index=None,
-                                frames_per_buffer=FRAME_SIZE)
 
-stream.start_stream()
+class Tuner(object):
+  
+    def __init__(self, dev_index=None):
+        # Initialize audio stream
+        self.stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
+                                        channels=1,
+                                        rate=FSAMP,
+                                        input=True,
+                                        output=False,
+                                        input_device_index=dev_index,
+                                        frames_per_buffer=FRAME_SIZE)
 
-# Create Hanning window function
-window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, SAMPLES_PER_FFT, False)))
 
-# Print initial text
-print('sampling at', FSAMP, 'Hz with max resolution of', FREQ_STEP, 'Hz')
+    def __del__(self):
+        # Cleanup audio stream
+        self.stop()
+        self.stream.close()
 
-# As long as we are getting data:
-while stream.is_active():
 
-    # Shift the buffer down and new data in
-    buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
-    buf[-FRAME_SIZE:] = np.frombuffer(stream.read(FRAME_SIZE), np.int16)
+    def go(self, verbose):
+        # Allocate space to run an FFT.
+        buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
+        num_frames = 0
 
-    # Run the FFT on the windowed buffer
-    fft = np.fft.rfft(buf * window)
+        # Start stream
+        self.stream.start_stream()
 
-    # Get frequency of maximum response in range
-    freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
+        # Create Hanning window function
+        window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, SAMPLES_PER_FFT, False)))
 
-    # Get note number and nearest note
-    n = freq_to_number(freq)
-    n0 = int(round(n))
+        # Log info
+        if verbose:
+            print(f'Sampling at {FSAMP} Hz with max resolution of {FREQ_STEP} Hz')
 
-    # Console output once we have a full buffer
-    num_frames += 1
+        while self.stream.is_active():
 
-    if num_frames >= FRAMES_PER_FFT:
-        print(' freq: {:7.2f} Hz   note: {:>3s} {:+.2f}'.format(freq, note_name(n0), n-n0), end='\r')
+            # Shift the buffer down and new data in
+            buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
+            buf[-FRAME_SIZE:] = np.frombuffer(self.stream.read(FRAME_SIZE), np.int16)
+
+            # Run the FFT on the windowed buffer
+            fft = np.fft.rfft(buf * window)
+
+            # Get frequency of maximum response in range
+            freq = (np.abs(fft[I_MIN:I_MAX]).argmax() + I_MIN) * FREQ_STEP
+
+            # Get note number and nearest note
+            n = freq_to_number(freq)
+            n0 = int(round(n))
+
+            # Console output once we have a full buffer
+            num_frames += 1
+
+            if num_frames >= FRAMES_PER_FFT:
+                print(' freq: {:7.2f} Hz   note: {:>3s} {:+.2f}'.format(freq, note_name(n0), n-n0), end='\r')
+
+
+    def stop(self):
+        self.stream.stop_stream()
+
